c1277bcb50fd73fa54ea97faffc81c6af30dbeb4
Ayomide Akintan <emzyakints2005@gmail.com>
2023-11-14 12:47:47 +0100

    Update

--- main.cpp
+++ main.cpp
@@ -7,12 +7,11 @@ int main()
     const int rows = 4;
     const int cols = 2;
     string participantInfo[rows][cols] =
-    {
-        {"Emmanuel", "2203827"},
-        {"Miracle", "2203817"},
-        {"Collins", "2203797"},
-        {"Moses", "2203833"}
-    };
+        {
+            {"Emmanuel", "2203827"},
+            {"Miracle", "2203817"},
+            {"Collins", "2203797"},
+            {"Moses", "2203833"}};
 
     string regNo;
     cout << "This is to find out if you're a participant of Dorcas Hall E103." << endl;
@@ -27,8 +26,12 @@ int main()
         if (regNo == participantInfo[i][1])
         {
             isMember = true;
+            
+            // this is to remove the user name from the array
+
             cout << "You're a member of Dorcas Hall room E103 and here are your room mates names:" << endl;
-            for (int a = 0; a < rows; ++a) {
+            for (int a = 0; a < rows; ++a)
+            {
                 cout << participantInfo[a][0] << endl;
             }
             break;
@@ -39,7 +42,6 @@ int main()
     {
         cout << "You're not a member of Dorcas Hall room E103." << endl;
     }
-
 
     return 0;
 }
